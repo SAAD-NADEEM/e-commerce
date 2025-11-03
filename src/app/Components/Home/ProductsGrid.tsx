342106d26522b35f180350a8aa00039046c9ebc4
SAAD-NADEEM <saadngaming99090@gmail.com>
2025-11-04 01:59:47 +0500

    ProductCardTypes interface error fixed

--- src/app/Components/Home/ProductsGrid.tsx
+++ src/app/Components/Home/ProductsGrid.tsx
@@ -1,7 +1,7 @@
-import { Best_sellers_type } from "@/app/Utils/interfaces";
+import { ProductCardTypes } from "@/app/Utils/interfaces";
 import ProductCard from "../UI/ProductCard";
 
-function ProductsGrid({data}: {data: Best_sellers_type[]}) {
+function ProductsGrid({data}: {data: ProductCardTypes[]}) {
   // console.log(data, "this is data!!")
   return (
     <div className="w-full  gap-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
